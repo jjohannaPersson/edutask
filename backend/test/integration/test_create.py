45d3e235368b605eb70a23b2ec094952a0b2fae1
jjohannaPersson <jjohanna.persson@live.se>
2022-04-22 15:59:27 +0200

    Updated integration tests

--- backend/test/integration/test_create.py
+++ backend/test/integration/test_create.py
@@ -17,7 +17,8 @@ class TestFileHandler:
                 "properties": {
                     "firstName": {
                         "bsonType": "string",
-                        "description": "the first name of a user must be determined"
+                        "description": "the first name of a user must be determined",
+                        "uniqueItems": True,
                     },
                     "lastName": {
                         "bsonType": "string",
@@ -61,55 +62,8 @@ class TestFileHandler:
         content = sut.create(data)
         assert content['firstName'] == data['firstName']
 
-    def test_create_valid2(self, sut):
-        """ test create valid input to db, only required properties """
-        data = {
-            "firstName": "John",
-            "lastName": "Doe",
-        }
-        content = sut.create(data)
-        assert content['firstName'] == data['firstName']
-
-    def test_create_notUnique_in_task(self, sut):
-        """ test create with not uniqueItems in task """
-        data = {
-            "firstName": "John",
-            "lastName": "Doe",
-            "task": [
-                "test",
-                "test"
-            ]
-        }
-
-        with pytest.raises(pymongo.errors.WriteError):
-            sut.create(data)
-
-    def test_create_notUnique_document(self, sut):
-        """ test create with not uniqueItems document"""
-        data = {
-            "firstName": "John",
-            "lastName": "Doe",
-            "task": [
-                "test",
-                "test1"
-            ]
-        }
-        sut.create(data)
-
-        with pytest.raises(pymongo.errors.WriteError):
-            sut.create(data)
-
-    def test_create_wrong_bson_type_only_req(self, sut):
-        """ test create with wrong bson type, only required properties """
-        data = {
-            "firstName": 1,
-            "lastName": "Doe",
-        }
-        with pytest.raises(pymongo.errors.WriteError):
-            sut.create(data)
-
-    def test_create_wrong_bson_type_all_properties(self, sut):
-        """ test create with wrong bson type, all properties """
+    def test_create_wrong_bson_type(self, sut):
+        """ test create with wrong bson type """
         data = {
             "firstName": 1,
             "lastName": "Doe",
@@ -121,29 +75,7 @@ class TestFileHandler:
         with pytest.raises(pymongo.errors.WriteError):
             sut.create(data)
 
-    def test_create_wrong_bson_type_all_properties(self, sut):
-        """ test create with wrong bson type, all properties, not unique """
-        data = {
-            "firstName": 1,
-            "lastName": "Doe",
-            "task": [
-                "test",
-                "test"
-            ]
-        }
-        with pytest.raises(pymongo.errors.WriteError):
-            sut.create(data)
-
-    def test_create_missing_property_only_req(self, sut):
-        """ test create with missing required property, without task """
-        data = {
-            "firstName": "John",
-        }
-
-        with pytest.raises(pymongo.errors.WriteError):
-            sut.create(data)
-
-    def test_create_missing_property_all_properties(self, sut):
+    def test_create_missing_property(self, sut):
         """ test create with missing required property """
         data = {
             "firstName": "John",
@@ -156,48 +88,18 @@ class TestFileHandler:
         with pytest.raises(pymongo.errors.WriteError):
             sut.create(data)
 
-    def test_create_missing_property_all_properties_not_unique(self, sut):
-        """ test create with missing required property, not unique """
+    def test_create_notUnique_document(self, sut):
+        """ test create with not unique firstName """
         data = {
             "firstName": "John",
+            "lastName": "Doe",
             "task": [
                 "test",
-                "test"
+                "test1"
             ]
         }
+        sut.create(data)
 
-        with pytest.raises(pymongo.errors.WriteError):
-            sut.create(data)
-
-    def test_create_wrong_bson_type_and_missing_property_only_req(self, sut):
-        """ test create with wrong bson type """
-        data = {
-            "firstName": 1,
-        }
-        with pytest.raises(pymongo.errors.WriteError):
-            sut.create(data)
-
-    def test_create_wrong_bson_type_and_missing_property(self, sut):
-        """ test create with wrong bson type and missing property """
-        data = {
-            "firstName": 1,
-            "task": [
-                "test",
-                "test2"
-            ]
-        }
-        with pytest.raises(pymongo.errors.WriteError):
-            sut.create(data)
-
-    def test_create_wrong_bson_type_and_missing_property_not_unique(self, sut):
-        """ test create with wrong bson type, missing property and not unique """
-        data = {
-            "firstName": 1,
-            "task": [
-                "test",
-                "test"
-            ]
-        }
         with pytest.raises(pymongo.errors.WriteError):
             sut.create(data)
 
